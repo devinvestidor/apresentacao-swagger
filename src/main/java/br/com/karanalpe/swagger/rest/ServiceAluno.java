4aab5974af5ec4e25b8709803a42ffb28c7b162a
Karan Alves Pereira <kpereira@geomais.com.br>
2018-05-17 20:23:38 -0300

    Renomeando service usuario para aluno

--- src/main/java/br/com/karanalpe/swagger/rest/ServiceAluno.java
+++ src/main/java/br/com/karanalpe/swagger/rest/ServiceAluno.java
@@ -14,17 +14,17 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/usuario")
-public class ServiceUsuario {
+@Path("/aluno")
+public class ServiceAluno {
 
-	static List<String> pessoas = new ArrayList<String>();
+	static List<String> alunos = new ArrayList<String>();
 
 	@GET
 	@Path("/nome/{nome}")
 	public Response buscarPorNome(@PathParam("nome") String nome) {
-		for (int i = 0; i < pessoas.size(); i++) {
-			if (pessoas.get(i).equals(nome)) {
-				return Response.status(200).entity(pessoas.get(i)).build();
+		for (int i = 0; i < alunos.size(); i++) {
+			if (alunos.get(i).equals(nome)) {
+				return Response.status(200).entity(alunos.get(i)).build();
 			}
 		}
 		return Response.status(200).entity("Pessoa não encontrada").build();
@@ -34,9 +34,9 @@ public class ServiceUsuario {
 	@Produces(MediaType.TEXT_PLAIN + "; charset=UTF-8")
 	@Path("/buscar")
 	public Response buscarPorParametro(@QueryParam("nome") String nome) {
-		for (int i = 0; i < pessoas.size(); i++) {
-			if (pessoas.get(i).equals(nome)) {
-				return Response.status(200).entity(pessoas.get(i)).build();
+		for (int i = 0; i < alunos.size(); i++) {
+			if (alunos.get(i).equals(nome)) {
+				return Response.status(200).entity(alunos.get(i)).build();
 			}
 		}
 		return Response.status(200).entity("Pessoa não encontrada").build();
@@ -45,7 +45,7 @@ public class ServiceUsuario {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN + "; charset=UTF-8")
 	public Response buscarTodos() {
-		return Response.status(200).entity(pessoas).build();
+		return Response.status(200).entity(alunos).build();
 	}
 
 	@PUT
@@ -56,32 +56,32 @@ public class ServiceUsuario {
 		String nomeAntigo = array[0];
 		String nomeNovo = array[1];
 
-		for (int i = 0; i < pessoas.size(); i++) {
-			if (pessoas.get(i).equals(nomeAntigo)) {
-				pessoas.set(i, nomeNovo);
+		for (int i = 0; i < alunos.size(); i++) {
+			if (alunos.get(i).equals(nomeAntigo)) {
+				alunos.set(i, nomeNovo);
 			}
 		}
 
-		return Response.status(200).entity(pessoas).build();
+		return Response.status(200).entity(alunos).build();
 	}
 
 	@DELETE
 	@Produces(MediaType.TEXT_PLAIN + "; charset=UTF-8")
 	public Response excluir(String nome) {
-		for (int i = 0; i < pessoas.size(); i++) {
-			if (pessoas.get(i).equals(nome)) {
-				pessoas.remove(i);
+		for (int i = 0; i < alunos.size(); i++) {
+			if (alunos.get(i).equals(nome)) {
+				alunos.remove(i);
 			}
 		}
 
-		return Response.status(200).entity(pessoas).build();
+		return Response.status(200).entity(alunos).build();
 	}
 
 	@POST
 	@Produces(MediaType.TEXT_PLAIN + "; charset=UTF-8")
 	public Response inserir(String nome) {
-		pessoas.add(nome);
-		return Response.status(200).entity(pessoas).build();
+		alunos.add(nome);
+		return Response.status(200).entity(alunos).build();
 	}
 
 }
